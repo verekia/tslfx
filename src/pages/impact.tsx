88f67f10975165908207dd30efaa56e5c89162aa
verekia <522007+verekia@users.noreply.github.com>
2024-12-27 11:14:33 +0700

    feat: improve impact

--- src/pages/impact.tsx
+++ src/pages/impact.tsx
@@ -2,74 +2,107 @@ import Canvas from '@/components/Canvas'
 import { impact } from '@/shaders'
 import { useFrame } from '@react-three/fiber'
 import { useControls } from 'leva'
-import { useMemo, useRef } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 import { Vector4 } from 'three'
 import { MeshBasicNodeMaterial } from 'three/webgpu'
 
 const defaultColor1 = new Vector4(1, 0.1, 0, 1)
 const defaultColor2 = new Vector4(1, 0.6, 0, 1)
-const defaultDuration = 0.7
+
 const Scene = () => {
-  const totalAnimationTime = useRef(0)
   const materialRef = useRef<MeshBasicNodeMaterial>(null)
 
-  const [{ scale, color1, color2, duration, time, autoplay }, setControls] =
-    useControls(() => ({
-      scale: { value: 1, min: 0, max: 100, step: 0.1 },
-      time: { value: 0, min: 0, max: 1, step: 0.01 },
-      color1: {
-        value: {
-          r: defaultColor1.x * 255,
-          g: defaultColor1.y * 255,
-          b: defaultColor1.z * 255,
-          a: defaultColor1.w,
-        },
+  const [
+    {
+      scale,
+      circleColor,
+      vesicaColor,
+      duration,
+      time,
+      autoplay,
+      vesicaCount,
+      circleSizeStart,
+      circleSizeEnd,
+      circleThickness,
+    },
+    setControls,
+  ] = useControls(() => ({
+    circleColor: {
+      value: {
+        r: defaultColor1.x * 255,
+        g: defaultColor1.y * 255,
+        b: defaultColor1.z * 255,
+        a: defaultColor1.w,
       },
-      color2: {
-        value: {
-          r: defaultColor2.x * 255,
-          g: defaultColor2.y * 255,
-          b: defaultColor2.z * 255,
-          a: defaultColor2.w,
-        },
+    },
+    vesicaColor: {
+      value: {
+        r: defaultColor2.x * 255,
+        g: defaultColor2.y * 255,
+        b: defaultColor2.z * 255,
+        a: defaultColor2.w,
       },
-      duration: { value: defaultDuration, min: 0, max: 3, step: 0.1 },
-      autoplay: { value: false },
-    }))
+    },
+    vesicaCount: { value: 6, min: 1, max: 15, step: 1 },
+    circleSizeStart: { value: 0, min: 0, max: 1, step: 0.01 },
+    circleSizeEnd: { value: 0.5, min: 0, max: 1, step: 0.01 },
+    circleThickness: { value: 0.07, min: 0, max: 1, step: 0.01 },
+    scale: { value: 1, min: 0, max: 100, step: 0.1 },
+    time: { value: 0, min: 0, max: 1, step: 0.01 },
+    duration: { value: 0.5, min: 0, max: 3, step: 0.1 },
+    autoplay: { value: false },
+  }))
 
   const { uniforms, nodes } = useMemo(
     () =>
       impact({
         scale: 10,
         time: 0,
-        duration: defaultDuration,
-        color1: defaultColor1,
-        color2: defaultColor2,
+        duration,
+        circleColor: defaultColor1,
+        vesicaColor: defaultColor2,
+        vesicaCount,
+        circleSizeStart,
+        circleSizeEnd,
+        circleThickness,
       }),
-    []
+    [vesicaCount]
   )
+
+  useEffect(() => {
+    if (materialRef.current) {
+      materialRef.current.needsUpdate = true
+    }
+  }, [vesicaCount])
 
   useFrame((_, delta) => {
     if (!autoplay) return
 
-    totalAnimationTime.current += delta
-    const normalizedTime = (totalAnimationTime.current % duration) / duration
-    uniforms.time.value = normalizedTime
+    uniforms.time.value += delta / duration
+
+    if (uniforms.time.value > 1) {
+      uniforms.seed.value = Math.round(Math.random() * 1000)
+      uniforms.time.value = 0
+    }
+
     if (autoplay) {
-      setControls({ time: normalizedTime })
+      setControls({ time: uniforms.time.value })
     }
   })
 
   uniforms.time.value = time
   uniforms.scale.value = scale
-  uniforms.color1.value.x = color1.r / 255
-  uniforms.color1.value.y = color1.g / 255
-  uniforms.color1.value.z = color1.b / 255
-  uniforms.color1.value.w = color1.a
-  uniforms.color2.value.x = color2.r / 255
-  uniforms.color2.value.y = color2.g / 255
-  uniforms.color2.value.z = color2.b / 255
-  uniforms.color2.value.w = color2.a
+  uniforms.circleColor.value.x = circleColor.r / 255
+  uniforms.circleColor.value.y = circleColor.g / 255
+  uniforms.circleColor.value.z = circleColor.b / 255
+  uniforms.circleColor.value.w = circleColor.a
+  uniforms.vesicaColor.value.x = vesicaColor.r / 255
+  uniforms.vesicaColor.value.y = vesicaColor.g / 255
+  uniforms.vesicaColor.value.z = vesicaColor.b / 255
+  uniforms.vesicaColor.value.w = vesicaColor.a
+  uniforms.circleSizeStart.value = circleSizeStart
+  uniforms.circleSizeEnd.value = circleSizeEnd
+  uniforms.circleThickness.value = circleThickness
 
   return (
     <mesh scale={5}>
--- src/shaders/impact.ts
+++ src/shaders/impact.ts
@@ -18,13 +18,18 @@ const premultiplyRgba = (color: ShaderNodeObject<UniformNode<Vector4>>) =>
   vec4(color.xyz.mul(color.w), color.w)
 
 type ImpactParams = {
-  color1?: Vector4
-  color2?: Vector4
+  circleColor?: Vector4
+  vesicaColor?: Vector4
   scale?: number
   time?: number
   aspect?: number
   rotation?: number
   duration?: number
+  seed?: number
+  vesicaCount?: number
+  circleSizeStart?: number
+  circleSizeEnd?: number
+  circleThickness?: number
 }
 
 const defaultParams: Required<ImpactParams> = {
@@ -32,9 +37,14 @@ const defaultParams: Required<ImpactParams> = {
   scale: 1,
   aspect: 1,
   rotation: 0,
-  color1: new Vector4(0, 0, 0, 1),
-  color2: new Vector4(1, 1, 1, 1),
+  circleColor: new Vector4(0, 0, 0, 1),
+  vesicaColor: new Vector4(1, 1, 1, 1),
   duration: 1,
+  seed: 0,
+  vesicaCount: 3, // Requires recompilation
+  circleSizeStart: 0.5,
+  circleSizeEnd: 1,
+  circleThickness: 0.2,
 }
 
 export const impact = (params: ImpactParams) => {
@@ -44,40 +54,49 @@ export const impact = (params: ImpactParams) => {
   const s = uniform(par.scale)
   const a = uniform(par.aspect)
   const r = uniform(par.rotation)
-  const c1 = uniform(par.color1)
-  const c2 = uniform(par.color2)
+  const cCol = uniform(par.circleColor)
+  const vCol = uniform(par.vesicaColor)
   const d = uniform(par.duration)
+  const seed = uniform(par.seed)
+  const css = uniform(par.circleSizeStart)
+  const cse = uniform(par.circleSizeEnd)
+  const ct = uniform(par.circleThickness)
 
   const p = uv().sub(0.5).mul(2).mul(s).mul(vec2(a, 1))
   // const transparent = vec4(0, 0, 0, 0)
 
-  const circleThickness = float(0.1)
-  const radius = t.pow(3)
-  const circle = sdCircle(p, circleThickness.oneMinus().sub(0.2).sub(radius))
+  const radius = t.pow(0.5)
+  const circleSize = css.add(cse.sub(css).mul(radius)).sub(ct)
+  const circle = sdCircle(p, circleSize)
     .abs()
-    .step(circleThickness)
+    .step(ct)
     .toVec4()
-    .mul(premultiplyRgba(c1))
+    .mul(premultiplyRgba(cCol))
 
-  const createVesica = (pos: ReturnType<typeof vec2>, seed: number) => {
+  const createVesica = (
+    pos: ReturnType<typeof vec2>,
+    se: ShaderNodeObject<UniformNode<number>> | ReturnType<typeof float>
+  ) => {
     const vesicaR = float(1).mul(t.oneMinus().mul(t).mul(2))
     const vesicaD = float(0.8).mul(t.oneMinus().mul(t).mul(2))
 
-    const rotatedPForVesica = rotate(pos, hash(seed).mul(2).sub(1).mul(PI)).add(
-      vec2(0, t)
+    const rotatedPForVesica = rotate(pos, hash(se).mul(2).sub(1).mul(PI)).add(
+      vec2(0, t.mul(0.9))
     )
     const vesica = sdVesica(rotatedPForVesica, vesicaR, vesicaD)
       .step(0.01)
       .toVec4()
-      .mul(premultiplyRgba(c2))
+      .mul(premultiplyRgba(vCol))
     return vesica
   }
 
-  const vesica1 = createVesica(p, 0)
-  const vesica2 = createVesica(p, 1)
-  const vesica3 = createVesica(p, 2)
+  let result = circle
 
-  const colorNode = circle.add(vesica1).add(vesica2).add(vesica3)
+  for (let i = 0; i < par.vesicaCount; i++) {
+    result = result.add(createVesica(p, seed.add(i)))
+  }
+
+  const colorNode = result
 
   return {
     uniforms: {
@@ -85,9 +104,13 @@ export const impact = (params: ImpactParams) => {
       scale: s,
       aspect: a,
       rotation: r,
-      color1: c1,
-      color2: c2,
+      circleColor: cCol,
+      vesicaColor: vCol,
       duration: d,
+      seed,
+      circleSizeStart: css,
+      circleSizeEnd: cse,
+      circleThickness: ct,
     },
     nodes: { colorNode },
   }
