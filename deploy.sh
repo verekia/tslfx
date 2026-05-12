7cda89a90444e3c7c28a2c6021a5af0cd9935080
verekia <522007+verekia@users.noreply.github.com>
2026-05-12 16:02:33 +0700

    perf: gzip image

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker buildx build --platform linux/arm64 --load -t verekia/tslfx .
-docker save -o /tmp/tslfx.tar verekia/tslfx
-scp /tmp/tslfx.tar midgar:/tmp/
-ssh midgar docker load --input /tmp/tslfx.tar
+docker save verekia/tslfx | gzip > /tmp/tslfx.tar.gz
+scp /tmp/tslfx.tar.gz midgar:/tmp/
+ssh midgar docker load --input /tmp/tslfx.tar.gz
 ssh midgar docker compose up -d tslfx
